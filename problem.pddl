50d5fff229327b31d0b66b0be24d0e1737b85e52
OscarMoliina <oscar.molina.sedano@estudiantat.upc.edu>
2023-01-19 12:39:11 +0100

    'FinalSoftware'

--- problem.pddl
+++ problem.pddl
@@ -1,21 +1,33 @@
 (define (problem rush_hour) (:domain rush_hour)
 (:objects
- R - hor
- C13 C23 C33 - casilla
- C12 C22 C32 - casilla
- C11 C21 C31 - casilla
+    R - hor
+    C61 C62 C63 C64 C65 C66 - casilla
+	C51 C52 C53 C54 C55 C56 - casilla
+	C41 C42 C43 C44 C45 C46 - casilla
+	C31 C32 C33 C34 C35 C36 - casilla
+	C21 C22 C23 C24 C25 C26 - casilla
+	C11 C12 C13 C14 C15 C16 - casilla
 )
 
 (:init
-    (ArribaDe C13 C12)(ArribaDe C12 C11)(ArribaDe C23 C22)(ArribaDe C22 C21)(ArribaDe C33 C32)(ArribaDe C32 C31)
-    (DerechaDe C31 C21)(DerechaDe C21 C11)(DerechaDe C32 C22)(DerechaDe C22 C12)(DerechaDe C33 C23)(DerechaDe C23 C13)
-    (En R C12)(En R C22)
-    (Libre C11)(Libre C13)(Libre C21)(Libre C31)(Libre C32)(Libre C33)(Libre C23)
-    (CabezaEn R C22)(ColaEn R C12)
-    (Extremo C13)(Extremo C12)(Extremo C11)(Extremo C23)(Extremo C33)(Extremo C32)(Extremo C31)(Extremo C21)
+    (ArribaDe C21 C11) (ArribaDe C31 C21) (ArribaDe C41 C31) (ArribaDe C51 C41) (ArribaDe C61 C51) 
+	(ArribaDe C22 C12) (ArribaDe C32 C22) (ArribaDe C42 C32) (ArribaDe C52 C42) (ArribaDe C62 C52) 
+	(ArribaDe C23 C13) (ArribaDe C33 C23) (ArribaDe C43 C33) (ArribaDe C53 C43) (ArribaDe C63 C53) 
+	(ArribaDe C24 C14) (ArribaDe C34 C24) (ArribaDe C44 C34) (ArribaDe C54 C44) (ArribaDe C64 C54) 
+	(ArribaDe C25 C15) (ArribaDe C35 C25) (ArribaDe C45 C35) (ArribaDe C55 C45) (ArribaDe C65 C55) 
+	(ArribaDe C26 C16) (ArribaDe C36 C26) (ArribaDe C46 C36) (ArribaDe C56 C46) (ArribaDe C66 C56) 
+	(DerechaDe C12 C11) (DerechaDe C22 C21) (DerechaDe C32 C31) (DerechaDe C42 C41) (DerechaDe C52 C51) 
+	(DerechaDe C13 C12) (DerechaDe C23 C22) (DerechaDe C33 C32) (DerechaDe C43 C42) (DerechaDe C53 C52) 
+	(DerechaDe C14 C13) (DerechaDe C24 C23) (DerechaDe C34 C33) (DerechaDe C44 C43) (DerechaDe C54 C53) 
+	(DerechaDe C15 C14) (DerechaDe C25 C24) (DerechaDe C35 C34) (DerechaDe C45 C44) (DerechaDe C55 C54) 
+	(DerechaDe C16 C15) (DerechaDe C26 C25) (DerechaDe C36 C35) (DerechaDe C46 C45) (DerechaDe C56 C55) 
+    (En R C21)(En R C22)
+    (Libre C23)(Libre C24)(Libre C25)(Libre C26)
+    (CabezaEn R C22)(ColaEn R C21)
+    (Extremo C21)(Extremo C26)
 )
 
 (:goal (and
-    (CabezaEn R C32)
+    (CabezaEn R C26)
 ))
 )
---
+++ rush_hour_creator.py
@@ -0,0 +1,107 @@
+import sys
+
+def generar_juego(r:list, horizontales: dict, verticales: dict, n: int):
+    with open('problem.pddl', 'w') as f:
+        f.write('(define (problem rush_hour) (:domain rush_hour)\n')
+
+        # ---- OBJECTS ---- 
+        
+        #Horizontales
+        f.write('(:objects\n')
+        f.write('\tR')
+        for i in range(1,len(horizontales)+1):
+            f.write(f' H{i}')
+        f.write(' - hor\n')
+
+        #Verticales
+        f.write('\t')
+        for i in range(1,len(verticales)+1):
+            f.write(f'V{i} ')
+        f.write('- ver\n')
+
+        #Casillas
+        for i in range(n,0,-1):
+            f.write('\t')
+            for j in range(1,n+1):
+                f.write(f'C{i}{j} ')
+            f.write('- casilla\n')
+        f.write(')\n\n')
+        
+        # ---- INIT ----
+        f.write('(:init\n\t')
+
+        #ArribaDe
+        for j in range(1,n+1):
+            for i in range(1,n):
+                f.write(f'(ArribaDe C{i+1}{j} C{i}{j}) ')
+            f.write('\n\t')
+        f.write('\n')
+        
+        #DerechaDe
+        for j in range(1,n):
+            f.write('\t')
+            for i in range(1,n):
+                f.write(f'(DerechaDe C{i}{j+1} C{i}{j}) ')
+            f.write('\n')
+        f.write('\n')
+
+        #Extremo
+        for i in range(1,n+1):
+            f.write('\t')
+            for j in range(1,n+1):
+                if i==1 or j==1 or i==6 or j==6:
+                    f.write(f'(Extremo C{i}{j}) ')
+            f.write('\n')
+        f.write('\n')
+
+        #Rojo
+        f.write('\t')
+        for i in r:
+            f.write(f'(En R C{i}) ')
+        f.write(f'(CabezaEn R C{r[-1]}) (ColaEn R C{r[0]})')
+        f.write('\n')
+
+        #Coches H
+        for c in horizontales.keys():
+            f.write('\t')
+            for casilla in horizontales[c]:
+                f.write(f'(En H{c} C{casilla}) ')
+            f.write(f'(CabezaEn H{c} C{horizontales[c][-1]}) (ColaEn H{c} C{horizontales[c][0]})\n')
+        
+        for c in verticales.keys():
+            f.write('\t')
+            for casilla in verticales[c]:
+                f.write(f'(En V{c} C{casilla}) ')
+            f.write(f'(CabezaEn V{c} C{verticales[c][-1]}) (ColaEn V{c} C{verticales[c][0]})\n')
+
+        #Libre
+        f.write('\t')
+        casillas = []
+        for i in range(1,n+1):
+            for j in range(1,n+1):
+                casillas.append(int(str(i)+str(j)))
+        for i in horizontales.values():
+            for j in i:
+                if j in casillas:
+                    casillas.remove(j)
+        for i in verticales.values():
+            for j in i:
+                if j in casillas:
+                    casillas.remove(j)
+        for i in r:
+            if i in casillas:
+                casillas.remove(i)
+        for i in casillas:
+            f.write(f'(Libre C{i}) ')
+
+        f.write('\n)\n')
+
+        # ---- GOAL ----
+        f.write('\n(:goal (and\n\t')
+        f.write(f'(CabezaEn R C{n-2}{n})')
+        f.write('\n))')
+
+        f.write('\n)')
+    f.close()
+
+generar_juego([41,42],{1:[11,12,13]},{1:[35,45]},6)
